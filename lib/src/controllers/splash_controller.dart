d83205033bc6fe1d8919b7d19ccafbbb898938e9
Leonardo Navarro <potty3800@gmail.com>
2020-11-27 17:13:41 -0500

    add: pantalla Splash junto con su controlador de estados

---
+++ lib/src/controllers/splash_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+
+class SplashController extends GetxController {
+  
+}
---
+++ lib/src/pages/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:preferencias_de_usuario/src/controllers/splash_controller.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder <SplashController>(
+          builder: (_)=>Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
