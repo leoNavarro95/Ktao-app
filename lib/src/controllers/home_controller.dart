eba02527e179d6a58b77d20c386fe5b99d40a5a9
Leonardo Navarro <potty3800@gmail.com>
2020-11-27 17:08:32 -0500

    add: callbacks para el ciclo de vida de Getx

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,9 +1,27 @@
 
 
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
 
 //este archivo va a contener las variables y funciones de la aplicacion que van a ser reactivas
 class HomeController extends GetxController{
+
+  //este metodo se ejecuta una sola vez cuando el widget ha sido cargado en memoria (OJO aun no es renderizado)
+  @override
+  onInit(){
+    super.onInit();
+    print('Es lo mismo que el initState()!!!!');
+    
+  }
+
+
+  //se ejecuta cuando se acaba de renderizar el widget
+  @override
+  onReady(){
+    super.onReady();
+    print('Widget renderizado!');
+  }
+
+  
 
   int _counter = 0;
 
