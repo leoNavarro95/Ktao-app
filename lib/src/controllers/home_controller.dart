e797b582eb9429f8ffd052ef5c6d4e9e20d5b67c
Leonardo Navarro <potty3800@gmail.com>
2020-11-27 16:37:53 -0500

    add: controladores de estado usando getx

---
+++ lib/src/controllers/home_controller.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:get/state_manager.dart';
+
+//este archivo va a contener las variables y funciones de la aplicacion que van a ser reactivas
+class HomeController extends GetxController{
+
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  increment(){
+    this._counter++;
+  }
+}
