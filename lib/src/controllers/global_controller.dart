b5f411d0397452cd4fa0f263959cf3e2445fb15f
Leonardo Navarro <potty3800@gmail.com>
2020-12-13 15:28:37 -0500

    [ADD] controlador global de la aplicacion usando GetX

---
+++ lib/src/controllers/global_controller.dart
@@ -0,0 +1,13 @@
+
+import 'package:get/state_manager.dart';
+
+class GlobalController extends GetxController {
+  
+  @override
+  void onInit() {
+    super.onInit();
+
+    print('[myCode] Global_Controller onInit()');
+  }
+
+}
