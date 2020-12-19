e75e1303dd6ffedc81fdba971258fe0c2f5e7076
Leonardo <potty3800@gmail.com>
2020-12-19 17:53:32 -0500

    Commit final para rama Getx, cambiando a GetPattern

--- lib/src/controllers/reactive_controller.dart
+++ lib/src/controllers/reactive_controller.dart
@@ -8,13 +8,24 @@ class ReactiveController extends GetxController{
   RxInt counter = 0.obs; 
   RxString date = '--:--'.obs;
 
+  RxList<String> items = List<String>().obs;
 
-  increment(){
+
+
+ void increment(){
     counter.value++;
   }
 
-  getDate(){
+  void getDate(){
     date.value = DateTime.now().toString();
+  }
+
+  void addItem(String item){
+    items.add(item);
+  }
+
+  void removeItem(index){
+    items.removeAt(index);
   }
 
 }
--- lib/src/pages/getx_reactive_page.dart
+++ lib/src/pages/getx_reactive_page.dart
@@ -17,28 +17,28 @@ class GetReactivePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Reactive page with Get'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Obx((){
-                print('counter');
-                return Text(_.counter.value.toString());
-            }),
-              Obx((){
-                print('date');
-                return Text(_.date.value);
-            })
-            ], 
+        body: Column(
+          children: [
+            reactiveWidget(_),
+            // SizedBox(height: 20),
+            Expanded(
+              child: listaElementos(_),
             ),
+          ],
         ),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
               heroTag: 'Increment',
+              
               child: Icon(Icons.add),
-              onPressed: ()=>_.increment()
+
+              onPressed: (){
+                _.increment();
+                _.getDate();
+                _.addItem(_.date.value);
+                }
               ),
 
               SizedBox(width: 10),
@@ -53,6 +53,84 @@ class GetReactivePage extends StatelessWidget {
       );
         }
         );
+
+  }
+
+
+  Widget reactiveWidget(ReactiveController _){
+    return Container(
+      // color: Colors.blueGrey,
+      child: Column(
+        
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Obx((){
+            print('counter');
+            return roundedBox(text: _.counter.value.toString(), bkgColor: Colors.blue[200]);
+        }),
+            SizedBox(width: 5),
+            Obx((){
+              print('date');
+              return roundedBox(text: _.date.value.substring(0,19), bkgColor: Colors.blue[200]);
+          }),
+
+          ],
+          ),
+          SizedBox(height: 5),
+          Divider(height: 2,thickness: 2,),
+        ], 
+        ),
+    );
+  }
+
+
+  Widget roundedBox({String text, Color bkgColor}){
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: EdgeInsets.all(5),
+        color: bkgColor,
+        child: Text(text),
+      )
+    );
+  }
+  
+  Widget listaElementos(ReactiveController _){
+
+    return Obx(
+      ()=> ListView.builder(
+      itemCount: _.items.length,
+      itemBuilder: (context, index){
+        return ListTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: 45),
+              roundedBox(text: index.toString(), bkgColor: Colors.blue[100]),
+              SizedBox(width: 5),
+              roundedBox(text: _.items[index].substring(0,19), 
+              bkgColor: Colors.blue[100]),
+            ],
+          ),
+          trailing: Container(
+            width: 40,
+            height: 40,
+            padding: EdgeInsets.all(0),
+            // color: Colors.green,
+            child: IconButton(
+              icon: Icon(Icons.delete_outline,color: Colors.blueAccent,),
+              onPressed: (){
+                _.removeItem(index);
+              }
+              ),
+          ),
+        );
+      }
+      )
+      );
 
   }
 
