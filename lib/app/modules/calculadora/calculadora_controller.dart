843a15fa4180afb0056dab616ee871bc225d2ff1
Leonardo <potty3800@gmail.com>
2020-12-20 15:21:12 -0500

    [ADD] ya se puede hallar el consumo como la resta de dos lecturas

--- lib/app/modules/calculadora/calculadora_controller.dart
+++ lib/app/modules/calculadora/calculadora_controller.dart
@@ -1,6 +1,28 @@
 import 'package:get/get.dart';
 
 class CalculadoraController extends GetxController{
-  
+
+  RxInt lectura1 = 0.obs;
+  // int get lectura1 => _lectura1.value;
+  // set lectura1(int dato){
+  // this._lectura1.value = dato;
+  // }
+
+  RxInt lectura2 = 0.obs;
+  // int get lectura2 => _lectura2.value;
+  // set lectura2(int dato){
+  // this._lectura2.value = dato;
+  // }
+
+  RxInt consumo = 0.obs;
+  // int get consumo => _consumo.value;
+  // set consumo(int dato){
+  // this._consumo.value = dato;
+  // }
+
+  void calcular(){
+    consumo.value = (lectura2.value - lectura1.value).abs(); //? OJO: abs() retorna el valor absoluto (|x|)
+    print('C[kWh]: $consumo');
+  }
 
 }
--- lib/app/modules/calculadora/calculadora_page.dart
+++ lib/app/modules/calculadora/calculadora_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:healthCalc/app/global_widgets/menu_lateral.dart';
+import 'package:healthCalc/app/modules/calculadora/calculadora_binding.dart';
 import 'package:healthCalc/app/modules/calculadora/calculadora_controller.dart';
 
 
 class CalculadoraPage extends StatelessWidget {
+
+  final _textCtrLectura1 = new TextEditingController();
+  final _textCtrLectura2 = new TextEditingController();
+  
+  final calcCtr = new CalculadoraController();
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CalculadoraController>(
@@ -14,10 +22,102 @@ class CalculadoraPage extends StatelessWidget {
           drawer: MenuLateral(),
           
           body: Center(
-          child: Text("Calculadora Page"),
+          child: _principal()
     ),
     );
       }
       );
   }
+
+  Widget _principal() {
+    return ListView(
+      padding: EdgeInsets.all(10.0),
+      children: <Widget>[
+        Text(
+          'Calcula el consumo en kWh',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        Divider(),
+        _creaFormulario(),
+        _crearBotonIMC(),
+        Divider(),
+        Obx(()=>Text("lect1: ${calcCtr.lectura1.value}  lect2: ${calcCtr.lectura2.value}  consumo: ${calcCtr.consumo.value}"),),
+        
+      ],
+    );
+  }
+
+  Widget _creaFormulario() {
+    return Column(
+      children: [
+        _campoTexto(
+            textController: _textCtrLectura1,
+            titulo: 'Lectura 1',
+            subTitulo: 'Introduzca la primera lectura',
+            ),
+        _campoTexto(
+            textController: _textCtrLectura2,
+            titulo: 'Lectura 2',
+            subTitulo: 'Introduzca la segunda lectura',
+            ),
+      ],
+    );
+  }
+
+  Widget _campoTexto(
+      {
+        TextEditingController textController,
+        String titulo,
+        String subTitulo,
+      }) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: TextField(
+          
+          inputFormatters: [FilteringTextInputFormatter.deny(RegExp('[ ,-]')),],
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          controller: textController,
+          decoration: InputDecoration(labelText: titulo, helperText: subTitulo),
+          onChanged: (val) {
+            // prefs.nombreUser = val;
+            // double valor = double.parse(val).toDouble();
+            int valor = int.parse(val).toInt();
+
+            if( titulo == "Lectura 1"){
+              //TODO: guardar Lectura 1
+              calcCtr.lectura1.value = valor;
+              calcCtr.calcular();
+              print("lectura1 = ${calcCtr.lectura1.value.toString()} ");
+            }
+            else if( titulo == "Lectura 2"){
+              //TODO: guardar lectura2
+              calcCtr.lectura2.value = valor;
+              calcCtr.calcular();
+              print("lectura2 = ${calcCtr.lectura2.value.toString()} ");
+            }
+          },
+        ),
+        );   
+  }
+
+  Widget _crearBotonIMC() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50.0),
+      child: ElevatedButton.icon(
+        // style: ButtonStyle(),
+        icon: Icon(Icons.calculate),
+        label: Text('Calcular'),
+        onPressed: () {
+          calcCtr.calcular();
+        },
+      ),
+    );
+  }
+
+
+
 }
