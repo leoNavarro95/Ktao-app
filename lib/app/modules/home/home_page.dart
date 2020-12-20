e0bee036790278d46678ebe31815c109610f241a
Leonardo <potty3800@gmail.com>
2020-12-20 07:58:25 -0500

    [ADD] menu lateral y appBar a homePage

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
+import 'package:healthCalc/app/global_widgets/menu_lateral.dart';
 
 import 'package:healthCalc/app/modules/home/home_controller.dart';
 
@@ -8,7 +9,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return GetBuilder<HomeController>(builder: (_){
-      return Scaffold(body: Text("Home page"));
+      return Scaffold(
+        drawer: MenuLateral(),
+        appBar: AppBar(title: Text('Inicio'), centerTitle: true,),
+        body: Text("Home page")
+        );
     });
   }
 }
