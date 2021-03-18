e43674d679409173c9b50eaae698db810e0fd1e7
Leonardo <potty3800@gmail.com>
2021-03-18 11:50:18 -0400

    [REFACTORING] Calculadora page

--- lib/app/modules/calculadora/calculadora_page.dart
+++ lib/app/modules/calculadora/calculadora_page.dart
@@ -10,141 +10,137 @@ import 'package:ktao/app/theme/text_theme.dart';
 import 'package:ktao/app/utils/math_util.dart';
 
 class CalculadoraPage extends StatelessWidget {
-
+  final calcCtr = Get.find<CalculadoraController>();
   @override
   Widget build(BuildContext context) {
-    final calcCtr = Get.find<CalculadoraController>();
-
-    return GetBuilder<CalculadoraController>(builder: (_) {
-      return WillPopScope(
-        onWillPop: onWillPop,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Calculadora'),
-          ),
-          drawer: MenuLateral(),
-          body: Center(child: _principal(calcCtr)),
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculadora'),
         ),
-      );
-    });
-  }
-
-  Future<bool> onWillPop() {
-    return myboolDialog(
-      titulo: '¿Desea cerrar la app?',
-      subtitulo: 'Presione Ok para cerrarla'
+        drawer: MenuLateral(),
+        body: _pageContent(calcCtr),
+      ),
     );
   }
 
-  Widget _principal(CalculadoraController calcCtr) {
+  /// onWillPop es un callBack que se ejecuta cuando se desea cerrar la pantalla
+  Future<bool> onWillPop() {
+    return myboolDialog(
+      titulo: '¿Desea cerrar la app?',
+    );
+  }
+
+  Widget _pageContent(CalculadoraController calcCtr) {
     return ListView(
       children: <Widget>[
-        SizedBox(height: 20),
-        Text(
-          'Calcula el costo en CUP',
-          textAlign: TextAlign.center,
-          style: TemaTexto().titulo,
-        ),
-        Divider(),
-        _creaFormulario(calcCtr),
-        Container(
-          color: Color.fromRGBO(10, 100, 180, 0.4),
-          child: Obx(
-            () => Text(
-              "${calcCtr.consumo.value} kWh \n${calcCtr.costo.value.toStringAsFixed(2)} CUP",
-              style: TemaTexto().tituloTarjeta,
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        Obx(() {
-          if (calcCtr.lectura1.value != null) {
-            return Tabla(
-              titleRow: ['Rango', 'Consumo', 'Precio', 'Importe'],
-              titleColor: Color.fromRGBO(120, 200, 220, 1),
-              primaryColor: Color.fromRGBO(0, 180, 210, 0.7),
-              secundaryColor: Color.fromRGBO(10, 100, 180, 0.3),
-              cuerpo: [
-                rangos, //rango
-                calcCtr.listConsumo, //consumo
-                precios, //precios por rango
-                calcCtr.listPrecio, //importe
-              ],
-            );
-          }
-          return Container();
-        }),
+        _title(),
+        _formulario(),
+        _calcResults(),
+        Obx(() => (calcCtr.lectura1.value != null) ? _tabla() : Container()),
       ],
     );
   }
 
-  Widget _creaFormulario(CalculadoraController calcCtr) {
-    List<Widget> campoTexto = [];
-    return Obx(() {
-      if (calcCtr.expanded.value) {
-        campoTexto = [
-          CampoTextoCalculadora(
-              textController: calcCtr.textCtrLectura1,
-              titulo: "Lectura 1",
-              calcCtr: calcCtr),
-          SizedBox(height: 10),
-          CampoTextoCalculadora(
-              textController: calcCtr.textCtrLectura2,
-              titulo: "Lectura 2",
-              calcCtr: calcCtr),
-          
-        ];
-      } else {
-        campoTexto = [
-          CampoTextoCalculadora(
-              textController: calcCtr.textCtrLectura1,
-              titulo: "Consumo",
-              calcCtr: calcCtr),
-          
-        ];
-      }
-
-      return Container(
-        padding: EdgeInsets.all(10),
-        color: Colors.blue[100],
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: campoTexto,
-            ),
-            _changeMultiLectura(calcCtr),
-          ],
-        ),
-      );
-    });
+  Widget _title() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text('Calcula el costo en CUP',
+              textAlign: TextAlign.center, style: TemaTexto().titulo),
+          Divider(),
+        ],
+      ),
+    );
   }
 
-  Widget _changeMultiLectura(CalculadoraController calcCtr) {
-    IconData myIcon = Icons.expand_more;
-    return Obx(() {
-      if (calcCtr.expanded.value) {
-        myIcon = Icons.expand_less;
-      } else {
-        myIcon = Icons.expand_more;
-      }
-
-      return Column(
-        children: [
-          SizedBox(height: 15),
-          IconButton(
-            icon: Icon(
-              myIcon,
-              color: Colors.black38,
-            ),
-            iconSize: 40,
-            onPressed: () {
-              calcCtr.expand();
-            },
+  //! TODO: BUG: solo actualiza la tabla los cambios en el campo lectura 1, campo lectura 2 cuando es editado no hace que cambie la tabla
+  Widget _formulario() {
+    return Obx(() => Container(
+          padding: EdgeInsets.all(10),
+          color: Colors.blue[100],
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: getCampoTexto(calcCtr.expanded.value),
+              ),
+              _buttonExpandLecturas()
+            ],
           ),
-        ],
-      );
-    });
+        ));
+  }
+
+  List<CampoTextoCalculadora> getCampoTexto(bool isExpanded) {
+    return isExpanded
+        ? [
+            CampoTextoCalculadora(
+                textController: calcCtr.textCtrLectura1,
+                titulo: "Lectura 1",
+                calcCtr: calcCtr),
+            CampoTextoCalculadora(
+                textController: calcCtr.textCtrLectura2,
+                titulo: "Lectura 2",
+                calcCtr: calcCtr),
+          ]
+        : [
+            CampoTextoCalculadora(
+                textController: calcCtr.textCtrLectura1,
+                titulo: "Consumo",
+                calcCtr: calcCtr),
+          ];
+  }
+
+  Container _calcResults() {
+    return Container(
+      color: Color.fromRGBO(10, 100, 180, 0.4),
+      child: Obx(
+        () => Text(
+          "${calcCtr.consumo.value} kWh \n${calcCtr.costo.value.toStringAsFixed(2)} CUP",
+          style: TemaTexto().tituloTarjeta,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Tabla _tabla() {
+    return Tabla(
+      titleRow: ['Rango', 'Consumo', 'Precio', 'Importe'],
+      titleColor: Color.fromRGBO(120, 200, 220, 1),
+      primaryColor: Color.fromRGBO(0, 180, 210, 0.7),
+      secundaryColor: Color.fromRGBO(10, 100, 180, 0.3),
+      cuerpo: [
+        rangos, //rango
+        calcCtr.listConsumo, //consumo
+        precios, //precios por rango
+        calcCtr.listPrecio, //importe
+      ],
+    );
+  }
+
+  Widget _buttonExpandLecturas() {
+    return Obx(() => Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: IconButton(
+                icon: getExpandIcon(calcCtr.expanded.value),
+                iconSize: 40,
+                onPressed: calcCtr.expand,
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Icon getExpandIcon(bool isExpanded) {
+    const iconColor = Colors.black38;
+    return isExpanded
+        ? Icon(Icons.expand_less, color: iconColor)
+        : Icon(Icons.expand_more, color: iconColor);
   }
 }
